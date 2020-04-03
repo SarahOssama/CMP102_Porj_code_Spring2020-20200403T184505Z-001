fc0bb2bbe5fd870c00de1e34c72e39cf562f73a6
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2020-04-03 21:33:00 +0200

    blala

--- CMP102_Porj_code_Spring2020/Restaurant/Events/ArrivalEvent.cpp
+++ CMP102_Porj_code_Spring2020/Restaurant/Events/ArrivalEvent.cpp
@@ -13,7 +13,8 @@ void ArrivalEvent::Execute(Restaurant* pRest)
 	// Then adds it to normal, vegan, or VIP order lists that you will create in phase1
 
 	
-	
+	//Hamadaaa
+
 	///For the sake of demo, this function will just create an order and add it to DemoQueue
 	///Remove the next code lines in phases 1&2
 	Order* pOrd = new Order(OrderID,OrdType);
